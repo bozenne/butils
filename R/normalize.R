bf6d28787a09ba1c7ffa59f555ec70aa123e8297
bozenne <broz@sund.ku.dk>
2017-04-25 14:27:32 +0200

    Correct normalize remove issue with the last value

--- R/normalize.R
+++ R/normalize.R
@@ -3,9 +3,9 @@
 ## author: Brice Ozenne
 ## created: apr 25 2017 (14:13) 
 ## Version: 
-## last-updated: apr 25 2017 (14:19) 
+## last-updated: apr 25 2017 (14:26) 
 ##           By: Brice Ozenne
-##     Update #: 7
+##     Update #: 14
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -15,15 +15,16 @@
 ## 
 ### Code:
 
-EDF <- function(x){
-    mean(X<=x)
+EDF <- function(X,x,n){
+    mean(X<=x)-1/(2*n)
 }
-
 erfinv <- function (x){
     qnorm((1 + x)/2)/sqrt(2)
 }
 
 #' @title Transform a variable to obtain approximate normality
+#' @description (Slow) implementation of the transformation described in Albada, 2007.
+#' 
 #' @param X the vector of values
 #' 
 #' @references Albada et al. Transformation of arbitrary distributions to the normal distribution with application to EEG test-retest reliability (2007, journal of Neuroscience Methods)
@@ -34,7 +35,7 @@ erfinv <- function (x){
 #' ## normal distribution ##
 #' X <- rnorm(n)
 #' Xnorm <- normalize(X)
-#' shapiro.test(Xnorm[!is.infinite(Xnorm)])
+#' shapiro.test(Xnorm)
 #' # plot(X, Xnorm)
 #'
 #' ## gamma distribution
@@ -43,13 +44,15 @@ erfinv <- function (x){
 #' # hist(X)
 #' 
 #' Xnorm <- normalize(X)
-#' shapiro.test(Xnorm[!is.infinite(Xnorm)])
-#' # hist(Xnorm[!is.infinite(Xnorm)])
-#' plot(X[!is.infinite(Xnorm)],Xnorm[!is.infinite(Xnorm)])
+#' shapiro.test(Xnorm)
+#' # hist(Xnorm)
+#' plot(X,Xnorm)
 #' @export
 normalize <- function(X){
+    n <- length(X)
+    
     sapply(X, function(x){
-        sqrt(2)*erfinv(2*EDF(x)-1)
+        sqrt(2)*erfinv(2*EDF(X,x,n)-1)
     })
 }
 
