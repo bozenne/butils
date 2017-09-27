765d95f0256073ec37cbc3eee1ef2babe1777d62
bozenne <broz@sund.ku.dk>
2017-09-27 09:59:15 +0200

    function to load at startup for boot2

---
+++ R/onload.R
@@ -0,0 +1,25 @@
+### onload.R --- 
+#----------------------------------------------------------------------
+## author: Brice Ozenne
+## created: sep 16 2017 (23:42) 
+## Version: 
+## last-updated: sep 16 2017 (23:44) 
+##           By: Brice Ozenne
+##     Update #: 4
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+
+## Functions not exported by boot
+boot_isMatrix <- get("isMatrix", envir = asNamespace("boot"), inherits = FALSE)
+boot_index.array <- get("index.array", envir = asNamespace("boot"), inherits = FALSE)
+boot_boot.return <- get("boot.return", envir = asNamespace("boot"), inherits = FALSE)
+
+
+#----------------------------------------------------------------------
+### onload.R ends here
