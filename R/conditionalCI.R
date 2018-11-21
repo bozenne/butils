642483b86d424fb6c52a77b82ce23d0268da114e
Brice Maxime Hugues Ozenne <hpl802@sund.root.ku.dk>
2018-11-21 10:33:57 +0100

    (1.3.5) add conditional CI

---
+++ R/conditionalCI.R
@@ -0,0 +1,422 @@
+### conditionalCI.R --- 
+##----------------------------------------------------------------------
+## Author: Brice Ozenne
+## Created: nov 20 2018 (14:43) 
+## Version: 
+## Last-Updated: nov 21 2018 (10:33) 
+##           By: Brice Ozenne
+##     Update #: 67
+##----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+##----------------------------------------------------------------------
+## 
+### Code:
+
+## * conditionalCI [documentation]
+#' @title Conditional confidence interval 
+#' @description Conditional confidence intervals for normally distributed random variables.
+#' @name conditionalCI
+#' 
+#' @param theta [numeric vector] the observed value(s) of the statistic.
+#' @param sigma [numeric vector] the variance of the statistic(s).
+#' @param threshold [numeric, >0] the threshold.
+#' @param conf.level [numeric 0-1] Conditional confidence level of the interval.
+#' @param method [character] the method used to compute the conditional confidence interval.
+#' So far only \code{"shortest"} is supported.
+#' @param trace [interger, 0-2] should a progress bar be displayed?
+#' @param ... additional arguments to be passed to \code{lavaSearch2:::.calcShortestCI}
+#' to specify the optimization method.
+#' 
+#' @details Compute the confidence interval of theta
+#' conditional on theta being greater (in absolute value) than the threshold.
+#' theta is assumed to be normally distributed with known variance sigma.
+#' The thresholding is applied after normalization (i.e. dividing theta by sigma).
+#' This corresponding to conditioning on a minimal significance level.
+#'
+#' @examples
+#' ci <- conditionalCI(theta = seq(3,5, by = 0.1), threshold = 2.9999)
+#' print(ci)
+#'
+#' autoplot(ci)
+#' 
+#' 
+
+
+## * conditionalCI [code]
+#' @rdname conditionalCI
+#' @export
+conditionalCI <- function(theta, threshold,
+                          sigma = 1, conf.level = 0.95, method = "shortest", 
+                          trace = length(theta)>1, ...){
+
+    ## ** normalize arguments
+    n.theta <- length(theta)
+    alpha <- 1 - conf.level
+    method <- match.arg(method, choices = c("shortest"))
+
+    if(n.theta!=length(sigma)){
+        if(length(sigma)==1){
+            sigma <- rep(sigma, n.theta)
+        }else{
+            stop("The length of argument \'theta\' does not match the length of argument \'sigma\' \n",
+                 "length(theta)=",length(theta),"\n",
+                 "length(sigma)=",length(sigma),"\n")
+        }            
+    }
+
+    if(any(abs(theta)<threshold)){
+        stop("Contradiction between argument \'theta\' and argument \'threshold\' \n",
+             "abs(theta) should be lower than threshold \n")
+    }
+    
+    ## ** compute CI
+    M.AR <- matrix(NA, nrow = n.theta, ncol = 5,
+                   dimnames = list(NULL, c("value","lower.left","lower.right","upper.left","upper.right")))
+    M.CI <- matrix(NA, nrow = n.theta, ncol = 3,
+                   dimnames = list(NULL, c("value","lower","upper")))
+    df.optim <- NULL
+    if(trace==1){pb <- utils::txtProgressBar(min = 0, max = n.theta, style = 3)}
+
+    for(iSeq in 1:n.theta){
+        iTheta <- as.double(theta[iSeq]/sigma[iSeq])
+        if(trace==1){utils::setTxtProgressBar(pb, iSeq)}        
+        if(trace>1){cat(iTheta," ")}
+
+        if(threshold==0){
+            
+            iRes <- list(AR = c(lower.left = qnorm(alpha/2), lower.right = 0, upper.left = 0, upper.right = qnorm( 1 - alpha/2)),
+                         CI = c(lower = iTheta + qnorm(alpha/2), upper = iTheta + qnorm( 1 - alpha/2)),
+                         optim = NULL)
+
+        }else if(method == "shortest"){
+
+            vec.init <- c(theta1 = iTheta,
+                          theta2 = iTheta,
+                          lower = iTheta + pnorm(0.025)*1,
+                          upper = iTheta + pnorm(0.975)*1)
+            if(iSeq>1){
+                ## vec.init["lower"] <- ls.CI[[iSeq-1]]["lower"]
+                vec.init["theta1"] <- df.optim[df.optim$param == "theta1" & df.optim$theta == theta[iSeq-1],"solution"]
+                ## vec.init <- ls.CI[[iSeq-1]][c("theta1","theta2","lower","upper")]
+            }
+            iRes <- .calcShortestCI(value = iTheta, threshold = threshold, alpha = alpha,
+                                    vec.init = vec.init,
+                                    ...)
+        }
+        M.CI[iSeq,] <- c(theta[iSeq], iRes$CI * sigma[iSeq])
+        M.AR[iSeq,] <- c(theta[iSeq], iRes$AR * sigma[iSeq])
+        df.optim <- rbind(df.optim,
+                          cbind(theta = theta[iSeq], iRes$optim)
+                          )
+    
+    }
+    
+    if(trace==1){close(pb)}
+    if(trace>1){cat("\n")}
+    
+    ## ** export
+    out <- list(CI = M.CI,
+                AR = M.AR,
+                optim = df.optim,
+                conf.level = conf.level,
+                method = method,
+                threshold = threshold)
+    class(out) <- "conditionalCI"
+    return(out)
+}
+
+## * print.conditionalCI
+#' @export
+print.conditionalCI <- function(x, ...){
+    return(print(stats::confint(x)))
+}
+
+## * confint.conditionalCI
+#' @export
+confint.conditionalCI <- function(object, parm, level = 0.95, ...){
+    return(as.data.frame(object$CI))
+}
+
+## * autoplot.conditionalCI
+#' @title Display Conditional Confidence Intervals
+#' @description Display conditional confidence intervals.
+#'
+#' @param object output of the function \code{conditionalCI}.
+#' @param value [logical] should the identity line be displayed (in blue)?
+#' @param unconditional.CI [logical] should the unconditional confidence intervals be displayed (in red)?
+#' @param plot [logical] should the graph be displayed in a graphical window?
+#' @param ... not used, for compatibility with the generic method.
+#' 
+#' @export
+autoplot.conditionalCI <- function(object, value = TRUE, unconditional.CI = TRUE, plot = TRUE, ...){
+
+    ## extract data
+    CI <- stats::confint(object)
+
+    ## create plot
+    gg <- ggplot2::ggplot(CI, aes_string(x = "value"))
+    gg <- gg + ggplot2::geom_ribbon(aes_string(ymin = "lower", ymax = "upper"))
+    if(value){
+        gg <- gg + ggplot2::geom_abline(intercept = 0, slope = 1, col = "blue")
+    }
+    if(unconditional.CI){
+        gg <- gg + ggplot2::geom_abline(intercept = qnorm(0.975), slope = 1, col = "red")
+        gg <- gg + ggplot2::geom_abline(intercept = qnorm(0.025), slope = 1, col = "red")
+    }
+
+    ## display
+    if(plot){
+        print(gg)
+    }
+
+    ## export
+    return(invisible(list(data = CI,
+                          plot = gg)))
+}
+
+## * .calcShortestCI
+.calcShortestCI <- function(value, threshold, alpha,
+                            vec.init,
+                            optimizer = "optim", method.optim = "L-BFGS-B", ...){
+
+    ## ** find theta1 and theta2
+    Q <- function(theta){ 2 - stats::pnorm(threshold + theta) - stats::pnorm(threshold - theta)	}
+    d_Q <- function(theta){ - stats::dnorm(threshold + theta) + stats::dnorm(threshold - theta)	}
+
+    fn1 <- function(theta){
+        stats::pnorm(threshold + theta) - stats::pnorm(threshold - theta) - (1 - alpha) * Q(theta)
+    }
+    d_fn1 <- function(theta){
+        stats::dnorm(threshold + theta) + stats::dnorm(threshold - theta) - (1 - alpha) * d_Q(theta)
+    }
+    fn2 <- function(theta){
+        2*stats::pnorm(theta - threshold) - 1 - (1 - alpha)* Q(theta)
+    }
+    d_fn2 <- function(theta){
+        2*stats::dnorm(theta - threshold) - (1 - alpha )* d_Q(theta)
+    }
+
+    if(optimizer == "optim"){
+
+        iOut <- stats::optim(par = vec.init["theta1"],
+                             fn = function(x){fn1(x)^2},
+                             gr = function(x){2*fn1(x)*d_fn1(x)},
+                             ## lower = 0,
+                             method = method.optim)
+        theta1.optim <- data.frame(solution = iOut$par,
+                                   objective = iOut$value,
+                                   iteration = iOut$counts["function"],
+                                   convergence = iOut$convergence,
+                                   message = iOut$message,
+                                   stringsAsFactors = FALSE)
+        theta1 <- as.double(iOut$par)
+
+        ## curve(fn2, 0,10)
+        ## method.optim <- "Nelder-Mead"
+        iOut <- stats::optim(par = vec.init["theta2"],
+                             fn = function(x){fn2(x)^2},
+                             gr = function(x){2*fn2(x)*d_fn2(x)},
+                             ## lower = theta1,
+                             method = method.optim)
+        theta2.optim <- data.frame(solution = iOut$par,
+                                   objective = iOut$value,
+                                   iteration = iOut$counts["function"],
+                                   convergence = iOut$convergence,
+                                   message = iOut$message,
+                                   stringsAsFactors = FALSE)
+        theta2 <- as.double(iOut$par)
+        
+    }else if(optimizer == "uniroot"){
+
+        iOut <- stats::uniroot(f = fn1, lower = 0, upper = threshold + 5*qnorm(1 - alpha))
+        theta1.optim <- data.frame(solution = iOut$root,
+                                   objective = iOut$f.root,
+                                   iteration = iOut$iter,
+                                   convergence = NA,
+                                   message = "NA",
+                                   stringsAsFactors = FALSE)
+        theta1 <- iOut$root
+        
+        iOut <- stats::uniroot(f = fn2, lower = theta1, upper = threshold + 5*qnorm(1 - alpha/2))
+        theta2.optim <- data.frame(solution = iOut$root,
+                                   objective = iOut$f.root,
+                                   iteration = iOut$iter,
+                                   convergence = NA,
+                                   message = "NA",
+                                   stringsAsFactors = FALSE)
+        theta2 <- iOut$root
+    }
+
+    ## ** Identify acceptance region (optional, formula 2)
+    theta <- value
+    theta.sign <- sign(theta)
+    theta.abs <- abs(theta)
+
+    if (theta.abs < theta1) { ## |theta| in [0,theta1]
+
+        lower.left <- theta - qnorm( 1 - alpha/2 *  Q(theta) )
+        if(lower.left>=-threshold){
+            lower.left <- NA
+            upper.left <- NA
+        }else{
+            upper.left <- -threshold
+        }
+        lower.right <- threshold
+        upper.right <- theta + qnorm( 1 - alpha/2 *  Q(theta) )
+        
+    }else if (theta.abs < theta2) { ## |theta| in [theta1,theta2]
+        
+        lower.left <- NA
+        upper.left <- NA
+        lower.right <- threshold
+        upper.right <- theta + qnorm( pnorm( threshold - theta ) + (1 - alpha) *  Q(theta) )
+        
+    } else {  ## |theta| in [theta2,Inf]
+        
+        lower.left <- NA
+        upper.left <- NA
+        lower.right <- theta - qnorm( .5 * ( 1 + (1 - alpha) *  Q(theta) ) )
+        upper.right <- theta + qnorm( .5 * ( 1 + (1 - alpha) *  Q(theta) ) )
+        
+    }
+
+    ## reconstruct
+    if (theta.sign %in% c(0,1)){
+        AR <- c(lower.left = lower.left, lower.right = lower.right, upper.left = upper.left, upper.right = upper.right)
+    }else if (theta.sign == -1){
+        AR <- c(lower.left = -upper.right, lower.right = -upper.left, upper.left = -lower.right, upper.right = -lower.left)
+    }
+
+    ## ** Identify CI
+    x <- value
+    x.sign <- sign(x)
+    x.abs <- abs(x)
+    
+    ## *** compute x1 and x2 according to formula (6-7)
+    ## x1 <- theta1 + qnorm ( pnorm(threshold - theta1) + (1 - alpha)*( 1 - pnorm(threshold + theta1) + 1 - pnorm(threshold - theta1) ) )
+    x1 <- threshold + 2*theta1
+    x2 <- 2*theta2 - threshold
+
+    ## *** lower CI, i.e. solve equation (5)
+    if (threshold < x.abs && x.abs < x1) { ## x in [threshold, x1]
+        f.lower <-  function(theta){
+            2 * (1 - stats::pnorm( x.abs - theta )) - alpha * Q(theta)
+            ## num <- -dnorm(threshold - theta) + (1 - alpha) * ( -dnorm(threshold + theta) + dnorm(threshold - theta) )
+            ## denum <- dnorm( qnorm( pnorm(threshold - theta) + (1 - alpha) * (1 - pnorm(threshold + theta) + 1 - pnorm(threshold - theta)) ) )
+            ## return(1 + num / denum)
+        }
+        d_f.lower <-  function(theta){
+            2 * stats::dnorm( x.abs - theta ) - alpha * d_Q(theta)
+        }
+    }else if (x.abs < x2) { ## x in [x1, x2]
+        f.lower <- function(theta){ 
+            stats::pnorm( x.abs - theta ) - stats::pnorm( threshold - theta ) - (1 - alpha) *  Q(theta)
+        }
+        d_f.lower <- function(theta){ 
+            - stats::dnorm( x.abs - theta ) + stats::dnorm( threshold - theta ) - (1 - alpha) *  d_Q(theta)
+        }
+    }else { ## x in [x2, Inf])
+        f.lower <- function(theta){
+            2 * stats::pnorm( x - theta ) - 1 - (1 - alpha) *  Q(theta)
+        }
+        d_f.lower <- function(theta){
+            - 2 * stats::dnorm( x - theta ) - (1 - alpha) *  d_Q(theta)
+        }
+    }
+        
+    ## *** upper CI    
+    f.upper <- function(theta){
+        2 * stats::pnorm(theta - x) - 1 - (1 - alpha) *  Q(theta)
+    }
+    d_f.upper <- function(theta){
+        2 * stats::dnorm(theta - x) - (1 - alpha) *  d_Q(theta)
+    }
+
+    if(optimizer == "optim"){
+
+        iOut <- stats::optim(par = vec.init["lower"],
+                             fn = function(x){f.lower(x)^2},
+                             gr = function(x){2*f.lower(x)*d_f.lower(x)},
+                             upper = value,
+                             method = method.optim)
+        lower.optim <- data.frame(solution = iOut$par,
+                                  objective = iOut$value,
+                                  iteration = iOut$counts["function"],
+                                  convergence = iOut$convergence,
+                                  message = iOut$message,
+                                  stringsAsFactors = FALSE)
+        lower <- as.double(iOut$par)
+        
+        iOut <- stats::optim(par = vec.init["upper"],
+                             fn = function(x){f.upper(x)^2},
+                             gr = function(x){2*f.upper(x)*d_f.upper(x)},
+                             lower = max(lower,value),
+                             method = method.optim)
+        upper.optim <- data.frame(solution = iOut$par,
+                                  objective = iOut$value,
+                                  iteration = iOut$counts["function"],
+                                  convergence = iOut$convergence,
+                                  message = iOut$message,
+                                  stringsAsFactors = FALSE)
+        upper <- as.double(iOut$par)
+        ## print(upper.optim$message)
+        ## Q(vec.init["lower"])^2
+    }else if(optimizer == "uniroot"){
+
+        iOut <- stats::uniroot(f = f.lower, lower = -theta1, upper = value)
+        lower.optim <- data.frame(solution = iOut$root,
+                                  objective = iOut$f.root,
+                                  iteration = iOut$iter,
+                                  convergence = NA,
+                                  message = "NA",
+                                  stringsAsFactors = FALSE)
+        lower <- as.double(iOut$root)
+        
+        iOut <- stats::uniroot(f = f.upper, lower = theta2, upper = threshold + 5*qnorm(1 - alpha/2))
+        upper.optim <- data.frame(solution = iOut$root,
+                                  objective = iOut$f.root,
+                                  iteration = iOut$iter,
+                                  convergence = NA,
+                                  message = "NA",
+                                  stringsAsFactors = FALSE)
+        upper <- as.double(iOut$root)
+        ## uses Chebyshev's_inequality
+        ## P[|X-mu|> 5*sd < 1/5^2]
+
+    }
+
+
+    ## M.print <- rbind(theta1 = unlist(theta1.optim[1:2]),
+                     ## theta2 = unlist(theta2.optim[1:2]),
+                     ## lower.optim = unlist(lower.optim[1:2]),
+                     ## upper.optim = unlist(upper.optim[1:2])
+                     ## )
+    ## if(any(M.print[,"value"]>1e-7)){
+        ## print(M.print)
+    ## }
+    
+    ## *** reconstruct
+    if (theta.sign %in% c(0,1)){
+        CI <- c(lower = lower, upper = upper)        
+    }else if (theta.sign == -1){
+        CI <- c(lower = -upper, upper = -lower)
+    }
+    
+    ## ** Export
+
+    return(list(AR = AR,
+                CI = CI,
+                optim = rbind(cbind(param = "theta1", theta1.optim),
+                              cbind(param = "theta2", theta2.optim),
+                              cbind(param = "lower", lower.optim),
+                              cbind(param = "upper", upper.optim))
+                ))
+}
+
+
+######################################################################
+### conditionalCI.R ends here
+
